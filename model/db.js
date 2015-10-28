62917765de5588cb548e1be215499e654e36846e
edgarw11 <edgarw11@gmail.com>
2015-10-28 01:04:46 -0200

    Change code to run on openShift

--- model/db.js
+++ model/db.js
@@ -1,2 +1,13 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/logisticdb');
+var db_name = 'logisticdb';
+
+//mongoose.connect('mongodb://localhost/logisticdb');
+
+//provide a sensible default for local development
+mongodb_connection_string = 'mongodb://127.0.0.1:27017/' + db_name;
+//take advantage of openshift env vars when available:
+if(process.env.OPENSHIFT_MONGODB_DB_URL){
+  mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + db_name;
+}
+
+mongoose.connect(mongodb_connection_string);
