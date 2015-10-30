b757d621f06053c587a5122e7d80d8b852b45558
edgarw11 <edgarw11@gmail.com>
2015-10-30 00:57:56 -0200

    fix db.js

--- model/db.js
+++ model/db.js
@@ -4,7 +4,7 @@ var username = 'admin';
 var password = 'jHDChYhBcl22';
 
 //mongoose.connect('mongodb://localhost/logisticdb');
-mongodb://username:password@host:port/database?options...
+
 //provide a sensible default for local development
 mongodb_connection_string = 'mongodb://' + username + ':' + password +'@127.0.0.1:27017/' + db_name;
 //take advantage of openshift env vars when available:
