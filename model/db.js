289589bd03fad59e76118f6571311e9521665ed5
edgarw11 <edgarw11@gmail.com>
2015-10-29 23:57:33 -0200

    add main to package.json. Add db credentials on mongo connection.

--- model/db.js
+++ model/db.js
@@ -1,10 +1,12 @@
 var mongoose = require('mongoose');
-var db_name = 'logisticdb';
+var db_name = 'logisticwebapp2';
+var username = 'admin';
+var password = 'aWuSUIIFMhfx';
 
 //mongoose.connect('mongodb://localhost/logisticdb');
-
+mongodb://username:password@host:port/database?options...
 //provide a sensible default for local development
-mongodb_connection_string = 'mongodb://127.0.0.1:27017/' + db_name;
+mongodb_connection_string = 'mongodb://' + username + ':' + password +'@127.0.0.1:27017/' + db_name;
 //take advantage of openshift env vars when available:
 if(process.env.OPENSHIFT_MONGODB_DB_URL){
   mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + db_name;
